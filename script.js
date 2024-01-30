a387873d78e6ec82a4bad507c3d589ffa1eca60d
Iryna Yelunina <lucky.asterisk@gmail.com>
2024-01-30 07:48:36 +0100

    commit

--- script.js
+++ script.js
@@ -16,6 +16,10 @@ printLibrary(library);
 function printLibrary(library) {  
     for (let i = 0; i < library.length; i++) {
          //TODO add all elements from library as list items to ul with id 'result'
+         const result = document.getElementById('result');
+         const li=document.createElement('li');
+         li.innerHTML = `<span>${result}</span>`;
+         result.append(li);
     }
 }
 
